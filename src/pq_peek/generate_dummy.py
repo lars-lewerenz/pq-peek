b163f3c1a8787df02f5dcb20b8f6dadf59583364
Lars Lewerenz <git.lars.lewerenz@gmail.com>
2026-01-31 12:02:32 +0100

    generate dummy file

---
+++ src/pq_peek/generate_dummy.py
@@ -0,0 +1,20 @@
+import numpy as np
+import polars as pl
+
+
+def create_dummy():
+    row_count = 1_000_000
+    df = pl.DataFrame({
+        "id": range(row_count),
+        "price": np.random.rand(row_count) * 100,
+        "category": np.random.choice(["Electronics", "Books", "Clothing"], row_count),
+        "is_available": np.random.choice([True, False], row_count),
+        "description": ["Product description placeholder"] * row_count
+    })
+
+    file_name = "data.parquet"
+    df.write_parquet(file_name)
+    print(f"Created '{file_name}' with {row_count} rows.")
+
+if __name__ == "__main__":
+    create_dummy()
